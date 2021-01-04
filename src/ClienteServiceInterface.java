9e5653b639b3433b5dd047164fe7831de464f09a
Alexandre Rodrigues <amfrodrigues97@gmail.com>
2021-01-04 10:28:13 +0000

    final commit for sprint 2 added save file received from cliente and then load to memory

--- src/ClienteServiceInterface.java
+++ src/ClienteServiceInterface.java
@@ -2,5 +2,5 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ClienteServiceInterface extends Remote {
-    boolean sendFile(StorageServiceInterface storageInterface,String fileName) throws RemoteException;
+    boolean sendFile(StorageServiceInterface storageInterface, String fileName) throws RemoteException;
 }
--- src/ResourceInfo.java
+++
@@ -1,9 +0,0 @@
-public class ResourceInfo {
-    // variables
-    public String resourceType;
-    public Float resourceTime;
-    public boolean repeatedCall;
-    public String cachedResource;
-    public Integer harRun;
-    public long resourceLength;
-}
